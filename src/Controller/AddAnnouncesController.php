af94c952bf4fec8a6addefcb3c10887b1953e282
clement <clem.jonckheere@gmail.com>
2023-02-18 12:58:45 +0100

    ajout image

--- src/Controller/AddAnnouncesController.php
+++ src/Controller/AddAnnouncesController.php
@@ -7,13 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Announce;
 use App\Form\AddAnnoncesType;
-use App\Repository\ProductRepository;
+use App\Repository\AnnounceRepository;
 use Symfony\Component\HttpFoundation\Request;
 
 class AddAnnouncesController extends AbstractController
 {
     #[Route('/add-announces', name: 'app_add_announces')]
-    public function index(Request $request, ProductRepository $productRepository): Response
+    public function index(Request $request, AnnounceRepository $announceRepository): Response
     {
 
         $announce = new Announce();
@@ -21,7 +21,7 @@ class AddAnnouncesController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $productRepository->save($announce);
+            $announceRepository->save($announce);
         }
         return $this->render('add_announces/index.html.twig', [
             'form'=> $form
